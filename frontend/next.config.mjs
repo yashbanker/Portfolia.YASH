c9e3a67dd9d11683d5f260845a3961e596332234
Yash Banker <yashbanker@gmail.com>
2026-06-21 20:45:53 +0530

    Revert static export for Netlify

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -2,10 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   eslint: { ignoreDuringBuilds: true },
-  output: 'export',
-  images: { 
-    unoptimized: true,
-    remotePatterns: [{ protocol: 'https', hostname: '**' }, { protocol: 'http', hostname: 'localhost' }] 
-  },
+  images: { remotePatterns: [{ protocol: 'https', hostname: '**' }, { protocol: 'http', hostname: 'localhost' }] },
 };
 export default nextConfig;
